ef66dccd9a70fc8f4a74817070844c3831b6bb31
Unknown <rggb@icomp.ufam.edu.br>
2017-12-15 10:08:37 -0500

    [INS] First Trial (inconsistent)

--- src/khash.hh
+++ src/khash.hh
@@ -38,7 +38,12 @@ const std::vector<char> CHAIN_CHARACTERS = {
     's',
     't',
     'u',
-    'v'
+    'v',
+    'w',
+    'x',
+    'y',
+    'z',
+    '='
 };
 
 struct HexDigit {
@@ -67,10 +72,11 @@ class KalousedHash {
     std::vector<char> hexChain;
     std::vector<HexDigit> hexMiscChar;
     std::set<char> usedChar;
+    std::ifstream file;
 
     bool __locked = false;
 
-    void __buildUnunseds()
+    static void __buildUnunseds(std::string& md5sum, std::vector<HexDigit>& hexMiscChar)
     {
         int sum = 0;
         int multiplication = 1;
@@ -89,19 +95,7 @@ class KalousedHash {
         std::sort(hexMiscChar.begin(), hexMiscChar.end());
     }
 
-public:
-    KalousedHash(std::string& md5sum)
-        : md5sum(md5sum)
-    {
-        __buildUnunseds();
-    };
-    KalousedHash(std::string&& md5sum)
-        : md5sum(md5sum)
-    {
-        __buildUnunseds();
-    };
-
-    void deleteFromHeap(char value)
+    static void __deleteFromHeap(char value, std::vector<HexDigit>& hexMiscChar)
     {
         HexDigit __pivot(0, value);
         auto searchIt = (std::lower_bound(hexMiscChar.begin(), hexMiscChar.end(), __pivot));
@@ -111,14 +105,7 @@ public:
         }
     }
 
-    void printHexMap()
-    {
-        for (auto& hex : hexMap) {
-            std::cout << hex.first << " -> " << hex.second << std::endl;
-        }
-    }
-
-    void buildHexMap()
+    void __buildHexMap()
     {
 
         if (__locked) {
@@ -138,7 +125,7 @@ public:
 
             hexChain.push_back(toUse);
             usedChar.insert(toUse);
-            deleteFromHeap(toUse);
+            __deleteFromHeap(toUse, hexMiscChar);
         }
 
         for (int i = 0, j = hexChain.size() - 1; i <= j; i++) {
@@ -147,8 +134,92 @@ public:
                 inverseHexMap.insert({ hexChain[0], hexChain[i] });
                 break;
             }
-            hexMap.insert({ hexChain[i], hexChain[i + 1] });
-            inverseHexMap.insert({ hexChain[i + 1], hexChain[i] });
+            int ip1 = i + 1;
+            hexMap.insert({ hexChain[i], hexChain[ip1] });
+            inverseHexMap.insert({ hexChain[ip1], hexChain[i] });
         }
+    }
+
+public:
+    KalousedHash(std::string&& filename)
+        : md5sum(StringUtils::md5_sum(filename.c_str()))
+        , file(filename, std::ios::binary)
+    {
+        __buildUnunseds(md5sum, hexMiscChar);
+        __buildHexMap();
+    };
+
+    void printHexMap()
+    {
+        for (auto& hex : hexMap) {
+            std::cout << hex.first << " -> " << hex.second << std::endl;
+        }
+    }
+
+    std::string getMD5()
+    {
+        return md5sum;
+    }
+
+    static void recover(std::string& filebase64, std::string& md5)
+    {
+
+        std::map<char, char> inverseHexMap;
+        std::set<char> usedChar;
+        std::vector<HexDigit> hexMiscChar;
+        std::vector<char> hexChain;
+        std::string outputBase64;
+        std::ifstream ifs(filebase64);
+        std::string base64((std::istreambuf_iterator<char>(ifs)),
+            (std::istreambuf_iterator<char>()));
+
+        __buildUnunseds(md5, hexMiscChar);
+
+        for (char c : md5) {
+            char toUse = '\0';
+
+            if (usedChar.find(c) != usedChar.end()) {
+                toUse = hexMiscChar.back().character;
+                hexMiscChar.pop_back();
+            } else {
+                toUse = c;
+            }
+
+            hexChain.push_back(toUse);
+            usedChar.insert(toUse);
+            __deleteFromHeap(toUse, hexMiscChar);
+        }
+
+        for (int i = 0, j = hexChain.size() - 1; i <= j; i++) {
+            if (i == j) {
+                inverseHexMap.insert({ hexChain[0], hexChain[i] });
+                break;
+            }
+            int ip1 = i + 1;
+            inverseHexMap.insert({ hexChain[ip1], hexChain[i] });
+        }
+
+        for (auto c : base64) {
+            auto mapIt = inverseHexMap.find(c);
+            outputBase64 += mapIt != inverseHexMap.end() ? mapIt->second : c;
+        }
+
+        StringUtils::base64_decode(outputBase64, filebase64 + "_out");
+    }
+
+    void encrypt(std::string&& outputFilename)
+    {
+        std::string fileBase64 = StringUtils::base64_encode(file);
+        std::string outputBase64;
+        std::ofstream ofs(outputFilename, std::ofstream::out);
+        std::transform(fileBase64.begin(), fileBase64.end(), fileBase64.begin(), ::tolower);
+
+        for (char c : fileBase64) {
+            auto mapIt = hexMap.find(c);
+            outputBase64 += mapIt != hexMap.end() ? mapIt->second : c;
+        }
+
+        ofs << outputBase64;
+        ofs.close();
     }
 };
--- src/main.cc
+++ src/main.cc
@@ -1,12 +1,39 @@
 #include "khash.hh"
 
-int main()
+int main(int argc, char* argv[])
 {
-    KalousedHash kh("9a3051eb1768badfb1412a2f35ff9b56");
-    kh.buildHexMap();
+
+    if (argc == 4) {
+        std::string option = std::string(argv[1]);
+        if (option == "e") {
+            KalousedHash kh(argv[2]);
+            std::cout << "Make sure to annotate the MD5 and DO NOT LOSE IT!" << std::endl;
+            std::cout << "MD5 : " << kh.getMD5();
+            kh.encrypt(argv[3]);
+            std::cout << "Successfully performed!" << std::endl;
+            std::cout << "Khash saved in the file '" << argv[2] << "'." << std::endl;
+        } else if (option == "r") {
+            std::string fName = argv[2];
+            std::string mSum = argv[3];
+            KalousedHash::recover(fName, mSum);
+            std::cout << "Successfully performed!" << std::endl;
+        } else {
+            std::cout << "Invalid option '" << option << "'.";
+            return 1;
+        }
+        return 0;
+    } else {
+        std::cout << "Invalid number of arguments (expected 3).";
+        return 1;
+    }
+
+    // std::cout << "This is your HexMap:" << std::endl;
     // kh.printHexMap();
-    std::ifstream file("KallousHash.txt", std::ios::binary);
-    std::string encode = StringUtils::base64_encode(file);
-    StringUtils::base64_decode(encode, "KallousHashDecoded.txt");
-    std::cout << StringUtils::md5_sum("KallousHashDecoded.txt");
+    // std::cout << std::endl;
+
+    // std::ifstream file("KallousHash.txt", std::ios::binary);
+    // std::string encode = StringUtils::base64_encode(file);
+
+    // StringUtils::base64_decode(encode, "KallousHashDecoded.txt");
+    // std::cout << StringUtils::md5_sum("KallousHashDecoded.txt");
 }
